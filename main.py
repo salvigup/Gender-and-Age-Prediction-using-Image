6b516582094e77384a7e2991788a5764f7507d38
salvigupta123 <guptasalvi008@gmail.com>
2022-01-09 12:33:05 +0530

    ex

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from AgeGender import scanImage, scanVideo
 
-st.title("My Mini Project")
+st.title("Age And Gender Prediction Using Image")
 st.image('aaaa.jpg', use_column_width=True)
 
 
@@ -14,23 +14,27 @@ sidebar.title('User Option')
 
 
 def introduction():
-    st.markdown("""
-         ## Heading Level 2
-         - Feature 1
-         - Feature 2
-         - Feature 3
-    """)
+    st.markdown(""" Age and gender information are very important for various real world applications, such as social understanding, biomet- rics, identity verification, video surveillance, human-computer interaction, electronic customer, crowd behavior analysis, on- line advertisement, item recommendation, and many more """)
+
+    st.subheader('Computer Vision')
+    st.markdown("""Computer vision is a field of artificial intelligence (AI) that enables computers and systems to derive meaningful information from digital images, videos and other visual inputs — and take actions or make recommendations based on that information.""")
+    st.image('computer.jpg', use_column_width=True)
     c1, c2 = st.columns(2)
 
-    c1.header("Column 1 Content")
-    c2.header("Column 2 Content")
+    c1.header("Uses")
+    st.markdown(""" 
+        1 - The objective of this project is to classify the gender and age of an individual in real time. 
+
+        2 - A real time Gender and Age prediction is a challenging problem when compared to other tasks in computer vision. ... The age classifier restores a whole number speaking to the age scope of the person.
+
+      """)
+    
 
 
-def execute():
-    st.subheader('project working here')
-
+def detect_image():
+        
     image_file = st.file_uploader("Upload An Image",type=['png','jpeg','jpg'])
-    img_name = st.text_input('age and gender.jpg')
+    img_name = st.text_input('image.jpg')
     btn = st.button("Predict")
 
     if btn and image_file:
@@ -49,12 +53,18 @@ def execute():
             print(e)
             st.error('Error Uploading Image')
 
+def detect_webcam():
+    run = st.checkbox('Start Detection')
+    if run:
+        scanVideo()
 
-options = ['Project Introduction', 'Execution']
+options = ['Project Introduction', 'Detect from Image', 'Detect from Webcam',]
 
 selOption = sidebar.selectbox("Select an OPtion", options)
 
 if selOption == options[0]:
     introduction()
 elif selOption == options[1]:
-    execute()
+    detect_image()
+elif selOption == options[2]:
+    detect_webcam()
